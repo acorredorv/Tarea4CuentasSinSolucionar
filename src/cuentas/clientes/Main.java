7b56c92689a6bd5b3ca72ab3d9762e2eadfa6e9e
acorredorv <alexcosas79@hotmail.com>
2025-05-20 22:11:11 +0200

    primera prueba

---
+++ src/cuentas/clientes/Main.java
@@ -0,0 +1,36 @@
+package cuentas.clientes;
+
+public class Main {
+    public Main() {
+        // Constructor vacío
+    }
+
+    public static void main(String[] args, float cantidad) {
+        Cuenta cuenta1 = new Cuenta("Rafael Nadal Parera", "1000-2365-85-1230456789", 2500.0, 0.0);
+        
+        operativa_cuenta(cuenta1, 2500);
+    }
+
+	/**
+	 * @param cuenta1
+	 * @param f 
+	 */
+	private static void operativa_cuenta(Cuenta cuenta1, float f) {
+		System.out.println("El saldo actual es: " + cuenta1.estado());
+        
+        try {
+            cuenta1.retirar(2300.0);
+            System.out.println("El saldo actual es: " + cuenta1.estado());
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        
+        System.out.println("Ingreso en cuenta");
+        try {
+            cuenta1.ingresar(695.0);
+            System.out.println("El saldo actual es: " + cuenta1.estado());
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+	}
+}
